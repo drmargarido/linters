e84206ee7ed52849bcecafbc22deb7e8ca449906
David Konsumer <konsumer@jetboystudio.com>
2020-05-10 17:52:47 -0700

    Remove --verbose from defauklt args

--- linter_standard.lua
+++ linter_standard.lua
@@ -3,12 +3,12 @@
 local config = require "core.config"
 local linter = require "plugins.linter"
 
--- add auto-fixing by adding '--fix' to your options
+-- add auto-fixing by adding '--fix' to your options, add '--verbose' to get the offending eslint rule
 config.standard_args = {}
 
 linter.add_language {
   file_patterns = {"%.js$"},
   warning_pattern = "[^:]:(%d+):(%d+): ([^\n]+)",
-  command = "standard --verbose $ARGS $FILENAME",
+  command = "standard $ARGS $FILENAME",
   args = config.standard_args
 }
