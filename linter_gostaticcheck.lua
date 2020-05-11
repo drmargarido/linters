c48d0b0f40be74287563b11552851d38a3a6f4ef
Blain Smith <rebelgeek@blainsmith.com>
2020-05-11 14:34:42 -0400

    include staticcheck as an availble go linter

---
+++ linter_gostaticcheck.lua
@@ -0,0 +1,8 @@
+local linter = require "plugins.linter"
+
+linter.add_language {
+  file_patterns = {"%.go$"},
+  warning_pattern = "([^:]+):(%d+):(%d+):%s?([^\n]*)",
+  command = "staticcheck ./...",
+  args = {}
+}
