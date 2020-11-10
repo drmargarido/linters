f37d57301b997a646ce0d31c83049358a2976be5
drmargarido <drmargarido@gmail.com>
2020-11-10 02:09:13 +0000

    Some versions of the nim check command only report the filename in the folder so the flag --listfullpaths was added to ensure the full path is present. Selected a different pattern according to the platform we are running.

--- linter_nim.lua
+++ linter_nim.lua
@@ -1,8 +1,16 @@
 local linter = require "plugins.linter"
 
+local pattern
+if PLATFORM == "Windows" then
+  -- TODO: For now in windows we only display single line warnings
+  pattern = [[$FILENAME%((%d+), (%d+)%)([^\n]+)]]
+else
+  pattern = [[$FILENAME%((%d+), (%d+)%)([^\n]+[^/]*)]]
+end
+
 linter.add_language {
   file_patterns = {"%.nim$", "%.nims$"},
-  warning_pattern = [[$FILENAME%((%d+), (%d+)%)([^\n]+[^\//]*)]],
-  command = "nim check $FILENAME 2>&1",
+  warning_pattern = pattern,
+  command = "nim --listfullpaths --stdout check $FILENAME",
   deduplicate = true
 }
